a000533f1a6acffa27f4d9a69dc074b065d55959
ivan <menja08@yahoo.com>
2020-01-28 13:57:16 +0100

    image slider working as expected

--- js/imageSlider.js
+++ js/imageSlider.js
@@ -4,31 +4,58 @@ var nextArrow = document.getElementById("nextArrow");
 
 // show first image, hide the rest
 var numberOfImages = houseId.children.length;
-//console.log(numberOfImages);
 
 for (i=1; i < numberOfImages; i++) {
     houseId.children[i].style.display = "none";
 }
 
-//backArrow.addEventListener("click", moveLeft);
+backArrow.addEventListener("click", moveLeft);
 nextArrow.addEventListener("click", moveRight);
 
+var counter = 0;
+var currentImage = houseId.children[counter];
+var nextImage = houseId.children[counter + 1];
+var lastImage = houseId.children[numberOfImages-1];
+
 function moveRight() {
-    console.log(22);
 
-    var currentImage = 0;
-    var firstImage = houseId.children[currentImage];
-    var lastImage = houseId.children[numberOfImages-1];
-    //var currentImage = firstImage;
-    var nextImage = currentImage + 1;
+    if (counter < numberOfImages-1) {
 
-    if (currentImage === firstImage) {
-	currentImage = nextImage;
+	counter = counter + 1;
+	nextImage = houseId.children[counter];
+	nextImage.style.display = "block";
+	currentImage = houseId.children[counter - 1];
+	currentImage.style.display = "none";
+	
+    } else {
+	counter = 0;
+	nextImage = houseId.children[counter];
+	nextImage.style.display = "block";
+	currentImage = houseId.children[numberOfImages - 1];
+	currentImage.style.display = "none";
+    }
+   
+}
+
+var backCounter = numberOfImages - 1;
+function moveLeft() {
+
+    if (counter > 0) {
+	
+	currentImage = houseId.children[counter];
+	currentImage.style.display = "none";
+	counter = counter - 1;
+	prevImage = houseId.children[counter];
+	prevImage.style.display = "block";
+    } else {
+	currentImage = houseId.children[counter];
+	currentImage.style.display = "none";
+	counter = numberOfImages - 1;
+	prevImage = houseId.children[counter];
+	prevImage.style.display = "block";
+	
     }
 
-    if (currentImage === lastImage) {
-	nextImage = firstImage;
-    }
 }
 
 
